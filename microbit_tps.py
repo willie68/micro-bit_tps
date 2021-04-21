caf354bd056ea93b329ec761e0cd3c72ed58bbc1
Klaas, Wilfried <w.klaas@gmx.de>
2021-04-21 10:30:09 +0200

    first implementation: ready for testing.

--- microbit_tps.py
+++ microbit_tps.py
@@ -1,56 +1,99 @@
-# Button A is PRG or S1 (p5)
-# Button B is SEL or S2 (p11)
-# output pins 
-#   0 pin12
-#   1 pin8
-#   2 pin2
-#   3 pin1
-# input pins
-#   0 pin16
-#   1 pin15
-#   2 pin14
-#   3 pin13
-#  a/d pins
-#  d/a pins
-#  servo pins
-#  ppm in pins
-#
-# p0  a/d           DOut 0
-# p1  a/d           DOut 1
-# p2  a/d           DOut 2
-# p3  LED Col 3 a/d A/D 0
-# p4  LED Col 1 a/d A/D 1
-# p5  Button A      PRG/S1
-# p6  LED Col 4 
-# p7  LED Col 2 
-# p8                D/A 0
-# p9                D/A 1
-# p10 LED Col 5 a/d
-# p11 Button B      SEL/S2
-# p12 reserved      DOut 3
-# p13               DIn 0
-# p14               DIn 1
-# p15               DIn 2
-# p16               DIn 3
-# p19 I2C           
-# p20 I2C           
-
 from microbit import *
+import music
 import gc
 gc.collect()
 
 CRLF='\r\n'
+#default filename for tps file
 TPS_FILENAME='microtps.bin'
-min_acc = -1024
-max_acc = 1024
-range_acc = max_acc - min_acc
-E2END=1024
 
+#program memory
+E2END=1024
 p=bytearray(E2END)
 
+# our sdtack for push and pull with the pointer
 stack=bytearray(16)
 stackp=0
 
+#array of sub routines
+subs=[0,0,0,0,0,0]
+
+wait=[1,2,5]
+Din=[pin13, pin14, pin15, pin16]
+Dout=[pin0, pin1, pin2, pin12]
+Aout=[pin8, pin9]
+Ain=[pin3, pin4]
+button=[button_a, button_b]
+images={
+    1: Image.HEART,
+    2: Image.HAPPY,
+    3: Image.SMILE,
+    4: Image.SAD,
+    5: Image.CONFUSED,
+    6: Image.ANGRY,
+    7: Image.ASLEEP,
+    8: Image.SURPRISED,
+    9: Image.SILLY,
+    10: Image.FABULOUS,
+    11: Image.MEH,
+    12: Image.YES,
+    13: Image.NO,
+    14: Image.CLOCK1, 
+    15: Image.CLOCK2, 
+    16: Image.CLOCK3, 
+    17: Image.CLOCK4, 
+    18: Image.CLOCK5, 
+    19: Image.CLOCK6, 
+    20: Image.CLOCK7, 
+    21: Image.CLOCK8, 
+    22: Image.CLOCK9, 
+    23: Image.CLOCK10, 
+    24: Image.CLOCK11, 
+    25: Image.CLOCK12,
+    26: Image.ARROW_N, 
+    27: Image.ARROW_NE, 
+    28: Image.ARROW_E, 
+    29: Image.ARROW_SE, 
+    30: Image.ARROW_S, 
+    31: Image.ARROW_SW, 
+    32: Image.ARROW_W, 
+    33: Image.ARROW_NW,
+    34: Image.TRIANGLE,
+    35: Image.TRIANGLE_LEFT,
+    36: Image.CHESSBOARD,
+    37: Image.DIAMOND,
+    38: Image.DIAMOND_SMALL,
+    39: Image.SQUARE,
+    40: Image.SQUARE_SMALL,
+    41: Image.RABBIT,
+    42: Image.COW,
+    43: Image.MUSIC_CROTCHET,
+    44: Image.MUSIC_QUAVER,
+    45: Image.MUSIC_QUAVERS,
+    46: Image.PITCHFORK,
+    47: Image.XMAS,
+    48: Image.PACMAN,
+    49: Image.TARGET,
+    50: Image.TSHIRT,
+    51: Image.ROLLERSKATE,
+    52: Image.DUCK,
+    53: Image.HOUSE,
+    54: Image.TORTOISE,
+    55: Image.BUTTERFLY,
+    56: Image.STICKFIGURE,
+    57: Image.GHOST,
+    58: Image.SWORD,
+    59: Image.GIRAFFE,
+    60: Image.SKULL,
+    61: Image.UMBRELLA,
+    62: Image.SNAKE,
+    63: Image.HEART_SMALL
+}
+
+# starting with the code
+def map(a,x1,y1,x2,y2):
+    return int((a - x1) * (y2 - x2) / (y1 - x1) + x2)
+    
 def writeln(msg):
     uart.write(msg)
     uart.write(CRLF)
@@ -77,7 +120,7 @@ def get_nib(pb,nib):
     if nib:
         return p[pb]&0x0F
     else:
-        return 
+        return (p[pb]>>4)&0x0F
 
 def set_nib(pb,nib,v):
     if nib:
@@ -123,6 +166,18 @@ def getNextChar():
         sleep(10)
     c=uart.read(1)
     return chr(c[0])
+
+
+music_note = ['c','c#','d','d#','e','f','f#','g','g#','a','a#','b']
+def getMidiNote(note):
+    if note >=32 and note <= 108:
+        tune = music_note[ note % 12] + chr(ord('2') + (int(note / 12))) + ":4"
+        writeln("tune:" + tune)
+        return tune
+    return "C0:1"
+
+def tansformAcc(value):
+    return map(value, -2000, 2000, 0, 256)
 
 def writeProgramSerial():
     display.show(Image.ARROW_N)
@@ -285,7 +340,7 @@ def prg():
 
         if button_b.is_pressed():
             moved=1
-            nib=(nib+1)%512
+            nib=(nib+1)%E2END
             PC=nib>>1
             if nib%4==0:
                 for i in range(2):
@@ -305,6 +360,16 @@ def prg():
         for i in range(4):
             display.set_pixel(4-i,nib%4,7*bool(get_nib(PC,nib%2)&(1<<i)))
         sleep(100)
+
+def init():
+    for i in range(E2END):
+        p[i] = 0xff
+    
+    for i in range(4):
+        Din[i].set_pull(Din[i].PULL_UP)
+
+    for i in range(6):
+        subs[i]=0
 
 def run():
     uart.init(baudrate=115200)
@@ -326,23 +391,20 @@ def run():
     Row=0
 
 # initialise the interpreter
-    wait=[1,2,5]
-    Din=[pin13, pin14, pin15, pin16]
-    Dout=[pin0, pin1, pin2, pin12]
-    Aout=[pin8, pin9]
-    Ain=[pin3, pin4]
-    button=[button_a, button_b]
-    for i in range(E2END):
-        p[i] = 0xff
-    
-    for i in range(4):
-        Din[i].set_pull(Din[i].PULL_UP)
 
 # loading the actual program file
     load(TPS_FILENAME)
-    writeProgramSerial()
+    # writeProgramSerial()
     display.clear()
 
+# scanning thru the program to get the sub routines
+    for i in range(E2END):
+        INST=hi_nib(i)
+        if INST== 0x0e:
+            DATA=lo_nib(i)
+            if DATA >=0x08 and DATA <= 0x0d:
+              subs[DATA-0x08] = i
+              
 # main lood
     while True:
         # getting instruction and data
@@ -350,7 +412,16 @@ def run():
         DATA=lo_nib(PC)
 
         if INST==0x00:
-            PC=PC
+            if DATA==0x01:
+                display.set_pixel(A,B,9)                
+            elif DATA==0x02:
+                display.set_pixel(A,B,0)                
+            elif DATA==0x03:
+                if A==0:
+                    display.clear()
+                else:
+                    image = images.get(A, Image.SAD)
+                    display.show(image)
 
         elif INST==0x01:
             for i in range(4):
@@ -393,8 +464,10 @@ def run():
                 display.set_pixel(9-DATA,Row,9*(A&0x01))
                 Dout[DATA-5].write_digital(bool(A&0x01))
             elif DATA==0x09:
+                AOut[0].set_analog_period(2)
                 AOut[0].write_analog(A*68)
             elif DATA==0x0A:
+                AOut[1].set_analog_period(2)
                 AOut[1].write_analog(A*68)
             elif DATA==0x0B:
                 AOut[0].set_analog_period(20)
@@ -415,7 +488,7 @@ def run():
 
         elif INST==0x06:
             if DATA==0x00:
-                A=Row
+                PC=PC
             elif DATA==0x01:
                 A=B
             elif DATA==0x02:
@@ -427,11 +500,13 @@ def run():
             elif DATA>=0x05 and DATA<=0x08:
                 A=(Din>>(DATA-5))&0x01
             elif DATA==0x09:
-                A=int(pin1.read_analog()/64)
+                A=int(Ain[0].read_analog()/64)
             elif DATA==0x0A:
-                A=int(pin2.read_analog()/64)
+                A=int(Ain[1].read_analog()/64)
             elif DATA==0x0E:
-                save()
+                A=E
+            elif DATA==0x0E:
+                A=F
             elif DATA==0x0F:
                 #Push to stack
                 stackp-=1
@@ -462,13 +537,15 @@ def run():
             elif DATA==0x0A:
                 A=A^0x0F
             elif DATA==0x0B:
-                A=A<<1
+                A=A%B
             elif DATA==0x0C:
-                A=A>>1
+                A=A+16*B
             elif DATA==0x0D:
-                A=(A<<1)|((A&0x08)>>3)
+                A=B-A
             elif DATA==0x0E:
-                A=(A>>1)|((A&0x01)<<3)
+                A=A>>1
+            elif DATA==0x0F:
+                A=A<<1
 
         elif INST==0x08:
             PAGE=DATA*16
@@ -490,15 +567,18 @@ def run():
                 continue
 
         elif INST==0x0C:
-            SKIP=A==0
-            if DATA==0x01:
+            if DATA==0x00:
+                SKIP=A==0
+            elif DATA==0x01:
                 SKIP=A>B
             elif DATA==0x02:
                 SKIP=A<B
             elif DATA==0x03:
                 SKIP=A==B
-            elif DATA>=0x04 and DATA<=0x0B:
-                SKIP=(Din>>DATA%4)&0x01==1*(DATA<0x08)
+            elif DATA>=0x04 and DATA<=0x07:
+                SKIP=(Din[DATA%4])&0x01==1
+            elif DATA>=0x08 and DATA<=0x0B:
+                SKIP=(Din[DATA%4])&0x01==0
             elif DATA>=0x0C and DATA<=0x0D:
                 SKIP=button[DATA-0x0C].is_pressed()
             elif DATA>=0x0E:
@@ -515,40 +595,52 @@ def run():
             if DATA==0x00:
                 PC=RET
                 continue
-            elif DATA>=0x01 and DATA<=0x03:
-                if DATA==0x01:
-                    acc_val = accelerometer.get_x()
-                elif DATA==0x02:
-                    acc_val = accelerometer.get_y()
-                elif DATA==0x03:
-                    acc_val = accelerometer.get_z()
-                acc_val = min(max(min_acc, acc_val), max_acc)
-                acc_val = ((acc_val - min_acc) / range_acc) * 16
-                A = int(acc_val)
+            elif DATA>=0x01 and DATA <= 0x06:
+                RET=PC
+                PC=subs[DATA-0x01]
+                continue
+            elif DATA==0x0F:
+                microbit.reset()
+        elif INST==0x0F:
+            if DATA==0x00:
+                A=int(Ain[0].read_analog()>>2)
+            elif DATA==0x01:
+                A=int(Ain[1].read_analog()>>2)
             elif DATA==0x04:
+                AOut[0].set_analog_period(2)
+                AOut[0].write_analog(A<<4)
+            elif DATA==0x05:
+                AOut[1].set_analog_period(2)
+                AOut[1].write_analog(A<<4)
+            elif DATA==0x06:
+                AOut[0].set_analog_period(20)
+                AOut[0].write_analog(int(A/2))
+            elif DATA==0x07:
+                AOut[1].set_analog_period(20)
+                AOut[1].write_analog(int(A/2))
+            elif DATA==0x08:
+                music.play(getMidiNote(A))
+            elif DATA==0x09:
+                #get accel
+                A=transformAcc(accelerometer.get_x())
+                E=transformAcc(accelerometer.get_y())
+                F=transformAcc(accelerometer.get_z())
+            elif DATA==0x0A:
                 if compass.is_calibrated == False:
                     compass.calibrate()
-                A = int((compass.heading() / 360) * 16)
-            elif DATA==0x05:
+                A = int(compass.heading() / 5)
+            elif DATA==0x0B:
                 mic_val = int((microphone.sound_level() / 255) * 16)
                 A = mic_val
-            elif DATA==0x06:
-                A = int((display.read_light_level() / 255) * 16)
-            elif DATA==0x07:
-                music.play(music_note[A%len(music_note)])
-            elif DATA==0x08:
-                music.play(music_tune[A%len(music_tune)])
-            elif DATA==0x09:
-               audio.play(audio_sound[A%len(audio_sound)])
-            elif DATA==0x0E:
-                A = pin_logo.is_touched() == False
-            elif DATA==0x0F:
+            elif DATA==0x0C:
+                A = display.read_light_level()
+            elif DATA==0x0D:
                 A = pin_logo.is_touched() == True
-        elif INST==0x0F:
-            PC = PC
-            
+           
+                
         PC=(PC+1)%E2END
 
+init()
 if button_b.is_pressed():
     prg()
 if button_a.is_pressed():
